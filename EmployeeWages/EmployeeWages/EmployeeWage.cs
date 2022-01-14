5c06278be418d74eeb39f2941e00ffdfe550d64c
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-14 11:47:08 +0530

    [Raja]:Added employeewage program implemented in EmployeeWagesProblem

---
+++ EmployeeWages/EmployeeWages/EmployeeWage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class EmployeeWage
+    {
+        public static void Display()
+        {
+            int isFullTime = 1;
+            int empHrs = 0;
+            int empRatePerHr = 20;
+            int employeeWage = 0;
+        Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isFullTime)
+            {
+                empHrs = 8;
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            employeeWage = empHrs * empRatePerHr;
+            Console .WriteLine("Employee Wage:"+employeeWage );
+        }
+    }
+}
