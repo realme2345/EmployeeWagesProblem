7bf57508fbd28fba161461cef18cfbf9fcaf7374
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-14 17:07:21 +0530

    [Raja]:Added program of employee is present or absent

---
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            Random  random = new Random();
+            int empCheck = random.Next(0, 2);
+            if( empCheck == isPresent)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            // EmployeeWage.Display();
+            //AddPartTimeWage.DisplayWage();
+            //CaseStatement.DisplayCase();
+            // EmployeeWagePerDays.DisplayWagePerDays();
+            EmployeeWagesPerHours.DisplayWagePerHours();
+        }
+
+
+    }
+}
