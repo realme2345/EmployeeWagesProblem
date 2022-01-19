5b22148a434716242d6591f50cf6c7d6cc6256d4
raja <95670571+realme2345@users.noreply.github.com>
2022-01-19 12:23:49 +0530

    Update Program.cs

--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -22,11 +22,13 @@ namespace EmployeeWages
             {
                 Console.WriteLine("Employee is absent");
             }
-            // EmployeeWage.Display();
-            //AddPartTimeWage.DisplayWage();
-            //CaseStatement.DisplayCase();
-            // EmployeeWagePerDays.DisplayWagePerDays();
-            EmployeeWagesPerHours.DisplayWagePerHours();
+            // UC2EmployeeWage.Display();
+            //UC3EmployeeWage.DisplayWage();
+            //UC4EmployeeWage.DisplayCase();
+            // UC5EmployeeWage.DisplayWagePerDays();
+           // UC6EmployeeWage.DisplayWagePerHours();
+            UC7EmployeeWage.DisplayUC7();
+           
         }
 
 
